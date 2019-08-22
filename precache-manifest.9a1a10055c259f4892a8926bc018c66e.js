bf2608eae31dc11cfecdaa4407d1c54bc9f88907
Robert Roksela <robert.roksela@gmail.com>
2019-08-22 18:10:06 +0100

    'deploy'

---
+++ precache-manifest.9a1a10055c259f4892a8926bc018c66e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "59aa0975c2a3813b3c47",
+    "url": "/css/about.135e904a.css"
+  },
+  {
+    "revision": "59aa0975c2a3813b3c47",
+    "url": "/js/about.1a563fe3.js"
+  },
+  {
+    "revision": "d2173f51cf6061c9a3c3",
+    "url": "/css/app.bd653e5a.css"
+  },
+  {
+    "revision": "d2173f51cf6061c9a3c3",
+    "url": "/js/app.81934347.js"
+  },
+  {
+    "revision": "9469b2c193064e3e4d42",
+    "url": "/js/chunk-vendors.7e356e48.js"
+  },
+  {
+    "revision": "be95eb11a35b910dbc0b0f145feb18d4",
+    "url": "/img/rrwd-logo.be95eb11.svg"
+  },
+  {
+    "revision": "9e9ebb7a03ddd5e92cfd3ddf7f1d55fa",
+    "url": "/img/checklist2a.9e9ebb7a.jpg"
+  },
+  {
+    "revision": "bcffb190bd29106abf564fca5407781b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6bbc38bc700e01c0ef510c4ca235379",
+    "url": "/css/bulma.min.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
